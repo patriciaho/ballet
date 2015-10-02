f3d1a65852598a2b537fa90af6a1d1760d473708
patriciaho <patricia.p.ho@gmail.com>
2015-10-02 03:27:19 -0700

    add del destroy

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,24 @@
 class Session < ActiveRecord::Base
-  has_many :student_sessions
+  has_many :student_sessions, dependent: :destroy
   has_many :students, :through => :student_sessions
 
   default_scope { order('date ASC') }
   scope :upcoming, -> { where('date >= ?', Date.today) }
+
+  def price_range
+    total_sessions = self.student_sessions.count
+    definite_sessions = self.student_sessions.where(:certainty => 'Definite').count
+    maybe_sessions = self.student_sessions.where(:certainty => 'Maybe').count
+    if definite_sessions == 0 && maybe_sessions != 0
+      price = (115/maybe_sessions).ceil
+      "$" + price.to_s
+    else
+      low_range = (115/total_sessions).ceil
+      high_range = (115/definite_sessions).ceil
+
+      return "$" + low_range.to_s if low_range == high_range
+
+      "$" + low_range.to_s + " - $" + high_range.to_s
+    end if total_sessions != 0
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  has_many :student_sessions
+  has_many :student_sessions, dependent: :destroy
   has_many :sessions, :through => :student_sessions
 
   default_scope { order('name ASC') }
