e78b1127b70aabc09a18e493d35c3ba63d11c347
Patricia Ho <patricia.p.ho@gmail.com>
2016-07-20 01:01:20 -0700

    add validations

--- app/models/student_session.rb
+++ app/models/student_session.rb
@@ -1,4 +1,6 @@
 class StudentSession < ActiveRecord::Base
   belongs_to :student
   belongs_to :session
+  validates :student, :presence => true ,  :message => 'Student cannot be blank, submission not saved'
+  validates :session, :presence => true ,  :message => 'Class cannot be blank, submission not saved'
 end
